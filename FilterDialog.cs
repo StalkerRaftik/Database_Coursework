5a75d83a914091bddcc3b485e2587bd62bdf5a38
WhileTrueDoEnd <altair-ivan@ya.ru>
2021-05-30 13:06:47 +1000

    v0.1

---
+++ FilterDialog.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _30_05_2021_Database_Coursework
+{
+    public partial class FilterDialog : Form
+    {
+        public FilterDialog()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ PlayerInformation.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _30_05_2021_Database_Coursework
+{
+    public class PlayerInformation
+    {
+        public string Login;
+        public int Age;
+        public int PassedLevels;
+        public string GameName;
+        public string Developer;
+        public int TimeSpent;
+        public string Contacts;
+        public DateTime FirstTimePlayed;
+        public DateTime LastTimePlayed;
+    }
+}
